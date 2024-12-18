fbdec7607f44f2b08f93f36806ba608bd08ac067
mm1545 <may.midassi15@gmail.com>
2024-12-19 00:58:30 +0100

    partie soap

--- intelligent_doc_mgmt/settings.py
+++ intelligent_doc_mgmt/settings.py
@@ -102,8 +102,12 @@ GRAPHENE = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'document_mgt',
+        'USER': 'postgres',
+        'PASSWORD':'Infooo150#',
+        'HOST':'localhost',
+        'PORT':5432,
     }
 }
 
--- intelligent_doc_mgmt/urls.py
+++ intelligent_doc_mgmt/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', GraphQLView.as_view(graphiql=True)),
     path('rest/', include('rest_api.urls')),
+    path('soap/', include('soap_api.urls')),
 ]
